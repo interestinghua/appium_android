a37026417203cc1173d398e444dd112a1541b0ab
MarsHua <interestinghua@126.com>
2018-07-25 18:20:39 +0800

    commit

--- test/functional/ajh_android/create_order_test.py
+++ test/functional/ajh_android/create_order_test.py
@@ -1,14 +1,4 @@
 import unittest
-from zipfile import ZipFile
-import json
-import os
-import random
-from time import sleep
-from dateutil.parser import parse
-
-from webdriver.applicationstate import ApplicationState
-from selenium.common.exceptions import NoSuchElementException
-
 from appium import webdriver
 import desired_capabilities
 
@@ -22,11 +12,54 @@ class AndroidAJHTests(unittest.TestCase):
         desired_caps = desired_capabilities.get_desired_capabilities('ApiDemos-debug.apk')
         self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
 
-    def tearDown(self):
-        self.driver.quit()
+    def get_window_size(self):
+        size = self.driver.get_window_size()
+
+        return size
+
+    def swipeUp(self, t=500, n=1):
+        size = get_window_size()
+        x1 = size['width'] * 0.5
+        y1 = size['height'] * 0.75
+        y2 = size['height'] * 0.25
+        for i in range(n):
+            self.driver.swipe(x1, y1, x1, y2, t)
+
+    def swipeDown(self, t=500, n=1):
+        size = get_window_size()
+        x1 = size['width'] * 0.5
+        y1 = size['height'] * 0.25
+        y2 = size['height'] * 0.75
+        for i in range(n):
+            self.driver.swipe(x1, y1, x1, y2, t)
+
+    def swipLeft(self, t=500, n=1):
+        size = get_window_size()
+        x1 = size['width'] * 0.75
+        y1 = size['height'] * 0.5
+        x2 = size['width'] * 0.25
+        for i in range(n):
+            self.driver.swipe(x1, y1, x2, y1, t)
+
+    def swipRight(self, t=500, n=1):
+        size = get_window_size()
+        x1 = size['width'] * 0.25
+        y1 = size['height'] * 0.5
+        x2 = size['width'] * 0.75
+        for i in range(n):
+            self.driver.swipe(x1, y1, x2, y1, t)
+
+    def get_element_location(self, element_id):
+        element = self.driver.find_element_by_accessibility_id(element_id)
+
+        left = element.location['x']
+        top = element.location['y']
+        right = element.location['x'] + element.size['width']
+        bottom = element.location['y'] + element.size['height']
+
+        return {left, top, right, bottom}
 
     def startActivity(self, activity):
-
         self.driver.start_activity('aihuishou.aijihui', activity)
         self.driver.press_keycode(3)
         self.driver.find_element_by_accessibility_id('create_tv_id')
@@ -43,7 +76,11 @@ class AndroidAJHTests(unittest.TestCase):
     def shake(self):
         self.driver.shake()
 
+    def tearDown(self):
+        self.driver.quit()
+
 
 if __name__ == "__main__":
-    suite = unittest.TestLoader().loadTestsFromTestCase(AppiumTests)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    # suite = unittest.TestLoader().loadTestsFromTestCase(AppiumTests)
+    # unittest.TextTestRunner(verbosity=2).run(suite)
+    print("run")
