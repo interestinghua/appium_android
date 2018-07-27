b0eef6823134e57ef3d43f99e83229a1fb0b61b4
MarsHua <interestinghua@126.com>
2018-07-27 18:33:54 +0800

    commit

--- test/functional/ajh_android/create_order_test.py
+++ test/functional/ajh_android/create_order_test.py
@@ -80,41 +80,83 @@ class AndroidAJHTests(unittest.TestCase):
         action.tap(create_order_manual).perform()
         sleep(3)
 
-        self.driver.find_element_by_android_uiautomator('new UiSelector().text("OPPO")').click()
+        activity = self.driver.current_activity
+        print 'activity: ', activity
+
+        # root = self.driver.find_elements_by_class_name('android.widget.FrameLayout')
+        # print 'root ', root
+
+        # category = self.driver.find_element_by_id('aihuishou.aijihui:id/text')
+        # print 'category ', category
+
+        brands = self.driver.find_elements_by_id('aihuishou.aijihui:id/typename')
+        print 'brands ', brands
+        # sometimes numbers shift
+        for el in brands:
+            text = el.text
+            if text == '苹果':
+                action.tap(el).perform()
+            elif text == 'OPPO':
+                pass
+
+        # 点击其他品牌
+        # self.driver.tap([(500, 500)])
+        # sleep(1)
+
+        # 点击苹果
+        self.driver.tap([(500, 700)])
+        sleep(1)
 
         # mobile_brand = self.driver.find_element_by_android_uiautomator('new UiSelector().className("android.widget.TextView").text("苹果")')
-        # mobile_brand = self.driver.find_element_by_xpath('//android.widget.TextView[contains(@text,"苹果")]')
+        # mobile_brand = self.driver.find_element_by_xpath('//hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.support.v4.view.ViewPager/android.widget.RelativeLayout/android.widget.ListView/android.widget.LinearLayout[2]/android.widget.LinearLayout/android.widget.TextView')
         # mobile_brand = self.driver.find_element_by_accessibility_id("苹果")
         # mobile_brand = self.driver.find_element_by_class_name("android.widget.TextView")
         # print '点击了 苹果手机', mobile_brand
         # action.tap(mobile_brand).perform()
-        sleep(2)
+        # sleep(2)
 
-        mobile_model = self.driver.find_element_by_android_uiautomator(
-            'new UiSelector().className("android.widget.TextView").text("苹果 iPhone 7 Plus")')
-        print '点击了 ', mobile_model
-        action.tap(mobile_model).perform()
+        # mobile_model = self.driver.find_element_by_android_uiautomator(
+        #     'new UiSelector().className("android.widget.TextView").text("苹果 iPhone 7 Plus")')
+        # print '点击了 ', mobile_model
+        # action.tap(mobile_model).perform()
+        # sleep(2)
+
+        # 选择7p
+        self.driver.tap([(800, 700)])
         sleep(2)
 
         # 选择手动下单原因 触摸失灵
         self.driver.tap([(600, 1100)])
-        sleep(1)
+        sleep(2)
 
         # 手动选择属性
-        prop01 = self.driver.find_element_by_android_uiautomator(
-            'new UiSelector().className("android.widget.TextView").text("A1700")')
-        print '点击了 ', prop01
-        action.tap(prop01).perform()
+        # prop01 = self.driver.find_element_by_android_uiautomator(
+        #     'new UiSelector().className("android.widget.TextView").text("其他")')
+        # print '点击了 ', prop01
+        # action.tap(prop01).perform()
+        #
+        # prop02 = self.driver.find_element_by_android_uiautomator(
+        #     'new UiSelector().className("android.widget.TextView").text("电池老化")')
+        # print '点击了 ', prop02
+        # action.tap(prop02).perform()
+        #
+        # prop03 = self.driver.find_element_by_android_uiautomator(
+        #     'new UiSelector().className("android.widget.TextView").text("拍摄功能正常")')
+        # print '点击了 ', prop03
+        # action.tap(prop03).perform()
 
-        prop02 = self.driver.find_element_by_android_uiautomator(
-            'new UiSelector().className("android.widget.TextView").text("电池老化")')
-        print '点击了 ', prop02
-        action.tap(prop02).perform()
+        props = self.driver.find_elements_by_class_name("android.widget.TextView")
+        print '属性 ', props
+        for el in props:
+            text = el.text
+            print 'prop text ', text
+            if text == '苹果':
+                action.tap(el).perform()
+            elif text == 'OPPO':
+                pass
 
-        prop03 = self.driver.find_element_by_android_uiautomator(
-            'new UiSelector().className("android.widget.TextView").text("拍摄功能正常")')
-        print '点击了 ', prop03
-        action.tap(prop03).perform()
+        # action.tap(mobile_brand).perform()
+        # sleep(2)
 
     def swipe_up(self, t=500, n=1):
         size = self.get_window_size()
